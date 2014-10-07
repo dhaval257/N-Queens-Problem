5befd0645d13c07b126e7971ac53e5e67eed2f7d
unknown <dhaval.damka@gmail.com>
2014-10-07 17:15:15 -0400

    Generating random initial states

--- src/is/program/nqueens/ChessBoard.java
+++ src/is/program/nqueens/ChessBoard.java
@@ -2,4 +2,41 @@ package is.program.nqueens;
 
 public class ChessBoard {
 
+	Integer[] formation;
+	int hValue;
+	
+	
+	
+	public ChessBoard(Integer[] formation) {
+		super();
+		this.formation = formation;
+	}
+	public Integer[] getFormation() {
+		return formation;
+	}
+	public void setFormation(Integer[] formation) {
+		this.formation = formation;
+	}
+	public int gethValue() {
+		return hValue;
+	}
+	public void sethValue(int hValue) {
+		this.hValue = hValue;
+	}
+	
+	public void pritnState() {
+		for (int i = 0; i < HillClimbing.BOARD_SIZE; i++) {
+			for (int j = 0; j < HillClimbing.BOARD_SIZE; j++) {
+				if(formation[j] == i){
+					System.out.print("| Q |");
+				}else{
+					System.out.print("|   |");
+				}
+				
+			}
+			System.out.println("");
+			//System.out.println(String.valueOf(formation[i]) + " ");
+		}
+	}
+	
 }
--- src/is/program/nqueens/HillClimbing.java
+++ src/is/program/nqueens/HillClimbing.java
@@ -1,8 +1,57 @@
 package is.program.nqueens;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+import javax.annotation.Generated;
+
 public class HillClimbing {
 
-		public static void main(String args[]){
-			System.out.println("Hi");
+	static int BOARD_SIZE = 0;
+
+	public static void main(String args[]) {
+		System.out.println("Enter board size");
+		
+		// Checking user input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		try {
+			BOARD_SIZE = Integer.parseInt(br.readLine());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+		System.out.println("Value enteres is : " + String.valueOf(BOARD_SIZE));
+
+		//
+		
+//		Integer[] checkFormation = new Integer[BOARD_SIZE];
+	//	checkFormation[0] = 1;checkFormation[1] = 2;checkFormation[2] = 0;
+		//checkFormation[3] = 4;checkFormation[4] = 3;
+		
+		
+		ChessBoard tempBoard = new ChessBoard(generateRandomInitialState());
+		tempBoard.pritnState();
+		
+		
+	}
+	
+	public static Integer[] generateRandomInitialState() {
+		
+		Integer[] toBeReturnedFormation = new Integer[BOARD_SIZE];
+		for(int i = 0 ; i < BOARD_SIZE ; i++){
+			
+					Random rand = new Random();
+
+		    // nextInt is normally exclusive of the top value,
+		    // so add 1 to make it inclusive
+		    int randomNum = rand.nextInt(BOARD_SIZE);
+		    System.out.println(String.valueOf(randomNum));
+		    toBeReturnedFormation[i] = randomNum;
+		}
+		return toBeReturnedFormation;
+	}
 }
