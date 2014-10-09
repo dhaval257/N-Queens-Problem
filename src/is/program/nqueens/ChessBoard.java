5695bf53b5abaf10f79a382a887195c73cd8bab6
unknown <dhaval.damka@gmail.com>
2014-10-09 02:03:29 -0400

    implemented main algo

--- src/is/program/nqueens/ChessBoard.java
+++ src/is/program/nqueens/ChessBoard.java
@@ -1,6 +1,11 @@
 package is.program.nqueens;
 
-public class ChessBoard {
+
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class ChessBoard implements Comparable<ChessBoard>{
 
 	Integer[] formation;
 	int hValue;
@@ -8,6 +13,7 @@ public class ChessBoard {
 	public ChessBoard(Integer[] formation) {
 		super();
 		this.formation = formation;
+		this.hValue = this.calculateHValue();
 	}
 
 	public Integer[] getFormation() {
@@ -28,7 +34,7 @@ public class ChessBoard {
 
 	public void pritnState() {
 		for (int i = 0; i < HillClimbing.BOARD_SIZE; i++) {
-			System.out.print("- "+ String.valueOf(i)+" -");
+			System.out.print("- " + String.valueOf(i) + " -");
 		}
 		System.out.println("");
 		for (int i = 0; i < HillClimbing.BOARD_SIZE; i++) {
@@ -45,14 +51,75 @@ public class ChessBoard {
 		}
 	}
 
-	
+	// Check if two given comlumn contains queens that are in conflict
 	public boolean areInConflict(int column1, int column2) {
 		if (this.formation[column1] == this.formation[column2]) {
 			return true;
-		}else if(Math.abs(column1-column2)==Math.abs(this.formation[column1] - this.formation[column2])){
+		} else if (Math.abs(column1 - column2) == Math
+				.abs(this.formation[column1] - this.formation[column2])) {
 			return true;
 		}
 		return false;
+	}
+
+	public int calculateHValue() {
+		int tempHValue = 0;
+		for (int i = 0; i < HillClimbing.BOARD_SIZE; i++) {
+			for (int j = 0; j < HillClimbing.BOARD_SIZE; j++) {
+				if (i != j && areInConflict(i, j)) {
+					tempHValue++;
+				}
+
+			}
+
+		}
+		return tempHValue / 2;// Because every pair was counted twice
+	}
+
+	public ChessBoard getBestChild() {
+		
+		ChessBoard tempChessBoard;
+		Integer[] tempFormation;
+		ChessBoard toBeReturnedChild;
+		PriorityQueue<ChessBoard> toBeReturnedChildren = new PriorityQueue<ChessBoard>();
+
+		for (int i = 0; i < HillClimbing.BOARD_SIZE; i++) {
+			for (int j = 0; j < HillClimbing.BOARD_SIZE; j++) {
+				// create new formation
+				tempFormation = new Integer[HillClimbing.BOARD_SIZE];
+				
+				//Copy from existing formation
+				tempFormation = this.getFormation().clone();
+				if(j != this.formation[i]){
+				tempFormation[i] = j;
+				tempChessBoard = new ChessBoard(tempFormation);
+				//tempChessBoard.pritnState();
+				toBeReturnedChildren.add(tempChessBoard);
+				}
+			}
+		}
+		//System.out.println("Total  size of queue is : " + String.valueOf(toBeReturnedChildren.size()));
+		/*for(ChessBoard singleBoad : toBeReturnedChildren){
+			System.out.println("Formation is :");
+			singleBoad.pritnState();
+			System.out.println("Its heuristic is : " + String.valueOf(singleBoad.gethValue()));
+		}*/
+		
+		toBeReturnedChild = toBeReturnedChildren.poll();
+		
+		if(toBeReturnedChild.gethValue() < this.gethValue()){
+			return toBeReturnedChild;	
+		}else{
+			return null;
+		}
+		
+	}
+
+	@Override
+	public int compareTo(ChessBoard o) {
+		// TODO Auto-generated method stub
+		return this.gethValue() - o.gethValue();
+		
 	}
 
 }
--- src/is/program/nqueens/HillClimbing.java
+++ src/is/program/nqueens/HillClimbing.java
@@ -3,19 +3,31 @@ package is.program.nqueens;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 
-import javax.annotation.Generated;
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+
+import org.paukov.combinatorics.Factory;
+import org.paukov.combinatorics.Generator;
+import org.paukov.combinatorics.ICombinatoricsVector;
+
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
 
 public class HillClimbing {
 
 	static int BOARD_SIZE = 0;
 	static int userInput1 = 0;
 	static int userInput2 = 0;
+	static ChessBoard retrivedBestChild;
+	
 
 	public static void main(String args[]) {
 		System.out.println("Enter board size");
-		
+
 		// Checking user input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -25,52 +37,75 @@ public class HillClimbing {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("Value enteres is : " + String.valueOf(BOARD_SIZE));
 
 		//
-		
-//		Integer[] checkFormation = new Integer[BOARD_SIZE];
-	//	checkFormation[0] = 1;checkFormation[1] = 2;checkFormation[2] = 0;
-		//checkFormation[3] = 4;checkFormation[4] = 3;
-		
-		
-		ChessBoard tempBoard = new ChessBoard(generateRandomInitialState());
-		tempBoard.pritnState();
-		try {
-			userInput1 = Integer.parseInt(br.readLine());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			userInput2 = Integer.parseInt(br.readLine());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		if(tempBoard.areInConflict(userInput1, userInput2)){
-			System.out.println("They are in Conflict !!");
-		}else{
-			System.out.println("They are not in Conflict !!");
-		}
-		
-		
-	}
-	
-	public static Integer[] generateRandomInitialState() {
-		
-		Integer[] toBeReturnedFormation = new Integer[BOARD_SIZE];
-		for(int i = 0 ; i < BOARD_SIZE ; i++){
-			
-					Random rand = new Random();
 
-		    // nextInt is normally exclusive of the top value,
-		    // so add 1 to make it inclusive
-		    int randomNum = rand.nextInt(BOARD_SIZE);
-		    System.out.println(String.valueOf(randomNum));
-		    toBeReturnedFormation[i] = randomNum;
+		// Integer[] checkFormation = new Integer[BOARD_SIZE];
+		// checkFormation[0] = 1;checkFormation[1] = 2;checkFormation[2] = 0;
+		// checkFormation[3] = 4;checkFormation[4] = 3;
+
+		ChessBoard tempBoard;
+		tempBoard = new ChessBoard(generateRandomInitialState());
+		// tempBoard.pritnState();
+
+		System.out.println("Heuristic value of this formation is :"
+				+ String.valueOf(tempBoard.gethValue()));
+
+		retrivedBestChild = tempBoard.getBestChild();
+
+		if (retrivedBestChild == null) {
+			System.out.println("Plateu");
+		} else {
+			retrivedBestChild.pritnState();
+			System.out.println("Improved heuristic is :"
+					+ String.valueOf(retrivedBestChild.gethValue()));
+		}
+
+		// //////////////////Main
+		// algorithm////////////////////////////////////////////////////////
+		// //////////////////////////////////////////////////////////////////////////////////////
+		retrivedBestChild = new ChessBoard(generateRandomInitialState());
+		int numOfRestart = 0;
+		int numOfSteps = 0;
+		// tempBoard.pritnState();
+
+		while (retrivedBestChild.gethValue() != 0) {
+			
+			retrivedBestChild = retrivedBestChild.getBestChild();
+
+			if (retrivedBestChild == null) {
+				System.out.println("Plateu");
+				numOfRestart++;
+				numOfSteps = 0 ;
+				retrivedBestChild = new ChessBoard(generateRandomInitialState());
+			} else {
+			//	retrivedBestChild.pritnState();
+				
+				numOfSteps++;
+			}
+		}
+		System.out.println("Number of restart required are : " + String.valueOf(numOfRestart));
+		System.out.println("Number of steps to climb are : " + String.valueOf(numOfSteps));
+		System.out.println("e e e e e");
+		// ///////////////////////////////////////////////////////////////////////////////////////
+		// ////////////////////////////////////////////////////////////////////////////////////////
+
+	}
+
+	public static Integer[] generateRandomInitialState() {
+
+		Integer[] toBeReturnedFormation = new Integer[BOARD_SIZE];
+		for (int i = 0; i < BOARD_SIZE; i++) {
+
+			Random rand = new Random();
+
+			// nextInt is normally exclusive of the top value,
+			// so add 1 to make it inclusive
+			int randomNum = rand.nextInt(BOARD_SIZE);
+			//System.out.println(String.valueOf(randomNum));
+			toBeReturnedFormation[i] = randomNum;
 		}
 		return toBeReturnedFormation;
 	}
